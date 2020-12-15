2dc67d635c1750d09850a5d00a8d534c7732cd11
Svitavsky <vadim.svitavsky@gmail.com>
2020-12-15 23:55:00 +0300

    Added chunks to seeder for better performance and execution time output

--- seeder.php
+++ seeder.php
@@ -4,37 +4,50 @@ include 'database.php';
 
 function seed($count)
 {
-    $connection = connection_open();
-    for ($i = 0; $i < $count; $i++) {
-        $name = generate_name();
-        $emailName = str_replace(' ', '_', $name);
-        $email = "{$emailName}@example.com";
-        $confirmed = rand(0, 1);
-        $expiry = rand(time() - 2629743, time() + 2629743);
-        $checked = rand(0, 1);
-        $valid = $checked === 1 ? rand(0, 1) : 0;
+    $start = microtime(true);
 
+    $chunk = 1000;
+    if ($count < $chunk) {
+        $chunk = $count;
+    }
+
+    $connection = connection_open();
+
+    toggle_keys($connection, 'users', false);
+    toggle_keys($connection, 'emails', false);
+
+    for ($i = 0; $i < $count / $chunk; $i++) {
+        $users = [];
+        $emails = [];
+        for ($j = 0; $j < $chunk; $j++) {
+            $name = generate_name();
+            $emailName = str_replace(' ', '_', $name);
+            $email = "{$emailName}@example.com";
+            $confirmed = rand(0, 1);
+            $expiry = rand(time() - 2629743, time() + 2629743);
+            $checked = rand(0, 1);
+            $valid = $checked === 1 ? rand(0, 1) : 0;
+
+            $users[] = "('{$name}','{$email}',{$expiry},{$confirmed})";
+            $emails[] = "('{$email}',{$checked},{$valid})";
+        }
+
+        $usersList = implode(',', $users);
         $usersQuery = <<<QUERY
 INSERT
 INTO users
     (username, email, validts, email_confirmed)
-VALUES (
-    '{$name}',
-    '{$email}',
-    {$expiry},
-    {$confirmed}
-)
+VALUES
+    {$usersList}
 QUERY;
 
+        $emailsList = implode(',', $emails);
         $emailsQuery = <<<QUERY
 INSERT
 INTO emails
     (email, checked, valid)
-VALUES (
-    '{$email}',
-    {$checked},
-    {$valid}
-)
+VALUES
+    {$emailsList}
 QUERY;
 
         if (!mysqli_query($connection, $usersQuery)) {
@@ -46,7 +59,14 @@ QUERY;
         }
     }
 
+    toggle_keys($connection, 'users', true);
+    toggle_keys($connection, 'emails', true);
+
     connection_close($connection);
+
+    $end = microtime(true);
+    $time = round($end - $start, 4);
+    echo "Inserted {$count} rows. It took {$time} seconds";
 }
 
 function generate_name()
@@ -67,6 +87,18 @@ function generate_name()
     }
 
     return "{$name} {$surname}";
+}
+
+function toggle_keys($connection, $table, $state)
+{
+    $action = $state ? 'ENABLE' : 'DISABLE';
+    $query = <<<QUERY
+ALTER TABLE {$table} {$action} KEYS;
+QUERY;
+
+    if (!mysqli_query($connection, $query)) {
+        die("Unable to {$action} keys for table {$table}!");
+    }
 }
 
 seed(1000000);
