535277894508796d951f6f65d16d6184f3e58cd9
Svitavsky <vadim.svitavsky@gmail.com>
2020-12-15 23:54:06 +0300

    Added database connection config, check_config function to validate it

--- database.php
+++ database.php
@@ -2,11 +2,15 @@
 
 function connection_open()
 {
-    $host = 'localhost';
-    $user = 'root';
-    $password = '';
-    $databaseName = 'mailer';
-    $connection = mysqli_connect($host, $user, $password, $databaseName);
+    if(file_exists('dbconfig.php')) {
+        $config = require_once 'dbconfig.php';
+    } else {
+        die('Отсутствует файл конфигурации БД - dbconfig.php, создайте его из примера - dbconfig_example.php');
+    }
+
+    check_config($config);
+
+    $connection = mysqli_connect($config['host'], $config['user'], $config['password'], $config['database']);
 
     if(mysqli_connect_errno()) {
         die('Connection failed: %s\n' . mysqli_connect_error());
@@ -17,4 +21,17 @@ function connection_open()
 
 function connection_close($connection) {
     mysqli_close($connection);
+}
+
+function check_config($config) {
+    $errorTemplate = 'Не указан параметр {param} в файле конфигурации!';
+    $configExample = require_once 'dbconfig_example.php';
+    $params = array_keys($configExample);
+
+    foreach($params as $param) {
+        if(!isset($config[$param])) {
+            $error = str_replace('{param}', $param, $errorTemplate);
+            die($error);
+        }
+    }
 }
